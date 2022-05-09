a9a981a7557448e795af2793ccb4802974f2d906
shbz1998 <shahbazhassankhan.malik.500@my.csun.edu>
2022-05-09 07:32:52 -0700

    added more hardware files

---
+++ hardwarefiles/LAB5_MALIK.gen/sources_1/bd/LAB5_MALIK/hdl/LAB5_MALIK_wrapper.vhd
@@ -0,0 +1,241 @@
+--Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2021.2.1 (win64) Build 3414424 Sun Dec 19 10:57:22 MST 2021
+--Date        : Thu Mar 17 20:37:45 2022
+--Host        : SHBZ running 64-bit major release  (build 9200)
+--Command     : generate_target LAB5_MALIK_wrapper.bd
+--Design      : LAB5_MALIK_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity LAB5_MALIK_wrapper is
+  port (
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    Pmod_out_0_pin10_io : inout STD_LOGIC;
+    Pmod_out_0_pin1_io : inout STD_LOGIC;
+    Pmod_out_0_pin2_io : inout STD_LOGIC;
+    Pmod_out_0_pin3_io : inout STD_LOGIC;
+    Pmod_out_0_pin4_io : inout STD_LOGIC;
+    Pmod_out_0_pin7_io : inout STD_LOGIC;
+    Pmod_out_0_pin8_io : inout STD_LOGIC;
+    Pmod_out_0_pin9_io : inout STD_LOGIC;
+    uart_rtl_rxd : in STD_LOGIC;
+    uart_rtl_txd : out STD_LOGIC
+  );
+end LAB5_MALIK_wrapper;
+
+architecture STRUCTURE of LAB5_MALIK_wrapper is
+  component LAB5_MALIK is
+  port (
+    Pmod_out_0_pin1_o : out STD_LOGIC;
+    Pmod_out_0_pin7_i : in STD_LOGIC;
+    Pmod_out_0_pin2_o : out STD_LOGIC;
+    Pmod_out_0_pin8_i : in STD_LOGIC;
+    Pmod_out_0_pin3_o : out STD_LOGIC;
+    Pmod_out_0_pin9_i : in STD_LOGIC;
+    Pmod_out_0_pin10_o : out STD_LOGIC;
+    Pmod_out_0_pin4_o : out STD_LOGIC;
+    Pmod_out_0_pin3_i : in STD_LOGIC;
+    Pmod_out_0_pin4_i : in STD_LOGIC;
+    Pmod_out_0_pin1_i : in STD_LOGIC;
+    Pmod_out_0_pin2_i : in STD_LOGIC;
+    Pmod_out_0_pin10_t : out STD_LOGIC;
+    Pmod_out_0_pin8_t : out STD_LOGIC;
+    Pmod_out_0_pin9_t : out STD_LOGIC;
+    Pmod_out_0_pin4_t : out STD_LOGIC;
+    Pmod_out_0_pin9_o : out STD_LOGIC;
+    Pmod_out_0_pin10_i : in STD_LOGIC;
+    Pmod_out_0_pin7_t : out STD_LOGIC;
+    Pmod_out_0_pin1_t : out STD_LOGIC;
+    Pmod_out_0_pin2_t : out STD_LOGIC;
+    Pmod_out_0_pin7_o : out STD_LOGIC;
+    Pmod_out_0_pin3_t : out STD_LOGIC;
+    Pmod_out_0_pin8_o : out STD_LOGIC;
+    uart_rtl_rxd : in STD_LOGIC;
+    uart_rtl_txd : out STD_LOGIC;
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC
+  );
+  end component LAB5_MALIK;
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  signal Pmod_out_0_pin10_i : STD_LOGIC;
+  signal Pmod_out_0_pin10_o : STD_LOGIC;
+  signal Pmod_out_0_pin10_t : STD_LOGIC;
+  signal Pmod_out_0_pin1_i : STD_LOGIC;
+  signal Pmod_out_0_pin1_o : STD_LOGIC;
+  signal Pmod_out_0_pin1_t : STD_LOGIC;
+  signal Pmod_out_0_pin2_i : STD_LOGIC;
+  signal Pmod_out_0_pin2_o : STD_LOGIC;
+  signal Pmod_out_0_pin2_t : STD_LOGIC;
+  signal Pmod_out_0_pin3_i : STD_LOGIC;
+  signal Pmod_out_0_pin3_o : STD_LOGIC;
+  signal Pmod_out_0_pin3_t : STD_LOGIC;
+  signal Pmod_out_0_pin4_i : STD_LOGIC;
+  signal Pmod_out_0_pin4_o : STD_LOGIC;
+  signal Pmod_out_0_pin4_t : STD_LOGIC;
+  signal Pmod_out_0_pin7_i : STD_LOGIC;
+  signal Pmod_out_0_pin7_o : STD_LOGIC;
+  signal Pmod_out_0_pin7_t : STD_LOGIC;
+  signal Pmod_out_0_pin8_i : STD_LOGIC;
+  signal Pmod_out_0_pin8_o : STD_LOGIC;
+  signal Pmod_out_0_pin8_t : STD_LOGIC;
+  signal Pmod_out_0_pin9_i : STD_LOGIC;
+  signal Pmod_out_0_pin9_o : STD_LOGIC;
+  signal Pmod_out_0_pin9_t : STD_LOGIC;
+begin
+LAB5_MALIK_i: component LAB5_MALIK
+     port map (
+      DDR_addr(14 downto 0) => DDR_addr(14 downto 0),
+      DDR_ba(2 downto 0) => DDR_ba(2 downto 0),
+      DDR_cas_n => DDR_cas_n,
+      DDR_ck_n => DDR_ck_n,
+      DDR_ck_p => DDR_ck_p,
+      DDR_cke => DDR_cke,
+      DDR_cs_n => DDR_cs_n,
+      DDR_dm(3 downto 0) => DDR_dm(3 downto 0),
+      DDR_dq(31 downto 0) => DDR_dq(31 downto 0),
+      DDR_dqs_n(3 downto 0) => DDR_dqs_n(3 downto 0),
+      DDR_dqs_p(3 downto 0) => DDR_dqs_p(3 downto 0),
+      DDR_odt => DDR_odt,
+      DDR_ras_n => DDR_ras_n,
+      DDR_reset_n => DDR_reset_n,
+      DDR_we_n => DDR_we_n,
+      FIXED_IO_ddr_vrn => FIXED_IO_ddr_vrn,
+      FIXED_IO_ddr_vrp => FIXED_IO_ddr_vrp,
+      FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
+      FIXED_IO_ps_clk => FIXED_IO_ps_clk,
+      FIXED_IO_ps_porb => FIXED_IO_ps_porb,
+      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+      Pmod_out_0_pin10_i => Pmod_out_0_pin10_i,
+      Pmod_out_0_pin10_o => Pmod_out_0_pin10_o,
+      Pmod_out_0_pin10_t => Pmod_out_0_pin10_t,
+      Pmod_out_0_pin1_i => Pmod_out_0_pin1_i,
+      Pmod_out_0_pin1_o => Pmod_out_0_pin1_o,
+      Pmod_out_0_pin1_t => Pmod_out_0_pin1_t,
+      Pmod_out_0_pin2_i => Pmod_out_0_pin2_i,
+      Pmod_out_0_pin2_o => Pmod_out_0_pin2_o,
+      Pmod_out_0_pin2_t => Pmod_out_0_pin2_t,
+      Pmod_out_0_pin3_i => Pmod_out_0_pin3_i,
+      Pmod_out_0_pin3_o => Pmod_out_0_pin3_o,
+      Pmod_out_0_pin3_t => Pmod_out_0_pin3_t,
+      Pmod_out_0_pin4_i => Pmod_out_0_pin4_i,
+      Pmod_out_0_pin4_o => Pmod_out_0_pin4_o,
+      Pmod_out_0_pin4_t => Pmod_out_0_pin4_t,
+      Pmod_out_0_pin7_i => Pmod_out_0_pin7_i,
+      Pmod_out_0_pin7_o => Pmod_out_0_pin7_o,
+      Pmod_out_0_pin7_t => Pmod_out_0_pin7_t,
+      Pmod_out_0_pin8_i => Pmod_out_0_pin8_i,
+      Pmod_out_0_pin8_o => Pmod_out_0_pin8_o,
+      Pmod_out_0_pin8_t => Pmod_out_0_pin8_t,
+      Pmod_out_0_pin9_i => Pmod_out_0_pin9_i,
+      Pmod_out_0_pin9_o => Pmod_out_0_pin9_o,
+      Pmod_out_0_pin9_t => Pmod_out_0_pin9_t,
+      uart_rtl_rxd => uart_rtl_rxd,
+      uart_rtl_txd => uart_rtl_txd
+    );
+Pmod_out_0_pin10_iobuf: component IOBUF
+     port map (
+      I => Pmod_out_0_pin10_o,
+      IO => Pmod_out_0_pin10_io,
+      O => Pmod_out_0_pin10_i,
+      T => Pmod_out_0_pin10_t
+    );
+Pmod_out_0_pin1_iobuf: component IOBUF
+     port map (
+      I => Pmod_out_0_pin1_o,
+      IO => Pmod_out_0_pin1_io,
+      O => Pmod_out_0_pin1_i,
+      T => Pmod_out_0_pin1_t
+    );
+Pmod_out_0_pin2_iobuf: component IOBUF
+     port map (
+      I => Pmod_out_0_pin2_o,
+      IO => Pmod_out_0_pin2_io,
+      O => Pmod_out_0_pin2_i,
+      T => Pmod_out_0_pin2_t
+    );
+Pmod_out_0_pin3_iobuf: component IOBUF
+     port map (
+      I => Pmod_out_0_pin3_o,
+      IO => Pmod_out_0_pin3_io,
+      O => Pmod_out_0_pin3_i,
+      T => Pmod_out_0_pin3_t
+    );
+Pmod_out_0_pin4_iobuf: component IOBUF
+     port map (
+      I => Pmod_out_0_pin4_o,
+      IO => Pmod_out_0_pin4_io,
+      O => Pmod_out_0_pin4_i,
+      T => Pmod_out_0_pin4_t
+    );
+Pmod_out_0_pin7_iobuf: component IOBUF
+     port map (
+      I => Pmod_out_0_pin7_o,
+      IO => Pmod_out_0_pin7_io,
+      O => Pmod_out_0_pin7_i,
+      T => Pmod_out_0_pin7_t
+    );
+Pmod_out_0_pin8_iobuf: component IOBUF
+     port map (
+      I => Pmod_out_0_pin8_o,
+      IO => Pmod_out_0_pin8_io,
+      O => Pmod_out_0_pin8_i,
+      T => Pmod_out_0_pin8_t
+    );
+Pmod_out_0_pin9_iobuf: component IOBUF
+     port map (
+      I => Pmod_out_0_pin9_o,
+      IO => Pmod_out_0_pin9_io,
+      O => Pmod_out_0_pin9_i,
+      T => Pmod_out_0_pin9_t
+    );
+end STRUCTURE;
---
+++ hardwarefiles/LAB5_MALIK.gen/sources_1/bd/LAB5_MALIK/ip/LAB5_MALIK_axi_uartlite_0_1/LAB5_MALIK_axi_uartlite_0_1_stub.vhdl
@@ -0,0 +1,51 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2.1 (win64) Build 3414424 Sun Dec 19 10:57:22 MST 2021
+-- Date        : Thu Mar 17 20:38:45 2022
+-- Host        : SHBZ running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/malik/Documents/ECE_520_PROJECTS/LAB5_MALIK/LAB5_MALIK.gen/sources_1/bd/LAB5_MALIK/ip/LAB5_MALIK_axi_uartlite_0_1/LAB5_MALIK_axi_uartlite_0_1_stub.vhdl
+-- Design      : LAB5_MALIK_axi_uartlite_0_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z010clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity LAB5_MALIK_axi_uartlite_0_1 is
+  Port ( 
+    s_axi_aclk : in STD_LOGIC;
+    s_axi_aresetn : in STD_LOGIC;
+    interrupt : out STD_LOGIC;
+    s_axi_awaddr : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s_axi_awvalid : in STD_LOGIC;
+    s_axi_awready : out STD_LOGIC;
+    s_axi_wdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    s_axi_wstrb : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s_axi_wvalid : in STD_LOGIC;
+    s_axi_wready : out STD_LOGIC;
+    s_axi_bresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s_axi_bvalid : out STD_LOGIC;
+    s_axi_bready : in STD_LOGIC;
+    s_axi_araddr : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s_axi_arvalid : in STD_LOGIC;
+    s_axi_arready : out STD_LOGIC;
+    s_axi_rdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    s_axi_rresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s_axi_rvalid : out STD_LOGIC;
+    s_axi_rready : in STD_LOGIC;
+    rx : in STD_LOGIC;
+    tx : out STD_LOGIC
+  );
+
+end LAB5_MALIK_axi_uartlite_0_1;
+
+architecture stub of LAB5_MALIK_axi_uartlite_0_1 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "s_axi_aclk,s_axi_aresetn,interrupt,s_axi_awaddr[3:0],s_axi_awvalid,s_axi_awready,s_axi_wdata[31:0],s_axi_wstrb[3:0],s_axi_wvalid,s_axi_wready,s_axi_bresp[1:0],s_axi_bvalid,s_axi_bready,s_axi_araddr[3:0],s_axi_arvalid,s_axi_arready,s_axi_rdata[31:0],s_axi_rresp[1:0],s_axi_rvalid,s_axi_rready,rx,tx";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "axi_uartlite,Vivado 2021.2.1";
+begin
+end;
